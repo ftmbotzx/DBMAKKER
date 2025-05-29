2eb7c9a3ecf0821e63ed57500f10c2dfc140c4ee
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-05-29 11:45:06 +0530

    Update cmd.py

--- plugins/cmd.py
+++ plugins/cmd.py
@@ -1,5 +1,60 @@
 from pyrogram import Client, filters
+import os
+import time
+import logging 
+import aiohttp
+import requests
+import asyncio
+import subprocess
+from pyrogram import Client, filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from info import LOG_CHANNEL, ADMINS, BOT_TOKEN
+
 
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("👋 Hello! Bot is running successfully!")
+
+
+@Client.on_message(filters.command("restart"))
+async def git_pull(client, message):
+    if message.from_user.id not in ADMINS:
+        return await message.reply_text("🚫 **You are not authorized to use this command!**")
+      
+    working_directory = "/home/ubuntu/URL-UPLOADER"
+
+    process = subprocess.Popen(
+        "git pull https://github.com/Anshvachhani998/SpotifyDL",
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+
+    )
+
+    stdout, stderr = process.communicate()
+    output = stdout.decode().strip()
+    error = stderr.decode().strip()
+    cwd = os.getcwd()
+    logging.info("Raw Output (stdout): %s", output)
+    logging.info("Raw Error (stderr): %s", error)
+
+    if error and "Already up to date." not in output and "FETCH_HEAD" not in error:
+        await message.reply_text(f"❌ Error occurred: {os.getcwd()}\n{error}")
+        logging.info(f"get dic {cwd}")
+        return
+
+    if "Already up to date." in output:
+        await message.reply_text("🚀 Repository is already up to date!")
+        return
+      
+    if any(word in output.lower() for word in [
+        "updating", "changed", "insert", "delete", "merge", "fast-forward",
+        "files", "create mode", "rename", "pulling"
+    ]):
+        await message.reply_text(f"📦 Git Pull Output:\n```\n{output}\n```")
+        await message.reply_text("🔄 Git Pull successful!\n♻ Restarting bot...")
+
+        subprocess.Popen("bash /home/ubuntu/SpotifyDL/start.sh", shell=True)
+        os._exit(0)
+
+    await message.reply_text(f"📦 Git Pull Output:\n```\n{output}\n```")
