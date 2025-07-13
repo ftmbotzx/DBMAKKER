db70168aadbf99671cfbf10d7f7f0c207614ce4b
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-13 13:25:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ import os
 SESSION = "spotifydl"
 API_ID = int(os.getenv("API_ID", "8012239"))
 API_HASH = os.getenv("API_HASH", "171e6f1bf66ed8dcc5140fbe827b6b08")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6538627123:AAEy4JiDRG_OrA3AJn8t-YIKy_N_3Z6BsNc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6538627123:AAHnqmZSyi5iIfEepdL82oPXtWp13FHsrU8")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002284232975"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002284232975"))
 PORT = int(os.getenv("PORT", "8080"))
