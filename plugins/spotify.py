8ede6698bc2d8289ad3a0f45d433ea411177b2ec
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-14 21:20:21 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -160,9 +160,9 @@ import asyncio
 @Client.on_callback_query(filters.regex(r"downloadall:(\d+)"))
 async def handle_download_all(client, callback_query):
     message_id = int(callback_query.data.split(":")[1])
-    user_id = callback_query.from_user.id
+    user_id, playlist_message_id = callback_query.from_user.id
 
-    data = song_cache.get(message_id)
+    data = song_cache.get(playlist_message_id)
     if not data:
         logging.info(f"take it data {data} //  {user_id} /// {message_id} ")
         await callback_query.answer("❌ Playlist data expired.", show_alert=True)
