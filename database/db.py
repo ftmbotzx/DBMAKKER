9f9fcfad6f63c9265770e6b223d7eeab00af53ea
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-17 12:45:50 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -8,8 +8,20 @@ from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from marshmallow.exceptions import ValidationError
 
-unpack_new_file_id = FileId
-
+def unpack_new_file_id(new_file_id):
+    """Return file_id, file_ref"""
+    decoded = FileId.decode(new_file_id)
+    file_id = encode_file_id(
+        pack(
+            "<iiqq",
+            int(decoded.file_type),
+            decoded.dc_id,
+            decoded.media_id,
+            decoded.access_hash
+        )
+    )
+    file_ref = encode_file_ref(decoded.file_reference)
+    return file_id, file_ref
 logger = logging.getLogger(__name__)
 
 class Database:
