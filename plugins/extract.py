e99d50b61cc1445dc9331449df899eab0acad208
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-07-18 13:51:54 +0530

    Update extract.py

--- plugins/extract.py
+++ plugins/extract.py
@@ -13,7 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__) 
 
 
 
